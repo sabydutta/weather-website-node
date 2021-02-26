c38767c1582c0b0d6c13d74e65ebea23e42c8c5b
Sabyasachi Dutta <sabyasachi.dutta@pearson.com>
2021-02-26 12:48:38 +0530

    Setup heroku

--- public/js/app.js
+++ public/js/app.js
@@ -11,7 +11,7 @@ weatherForm.addEventListener('submit', (e) => {
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''
 
-    fetch('http://localhost:3000/weather?address=' + encodeURIComponent(location))
+    fetch('/weather?address=' + encodeURIComponent(location))
         .then((response) => {
             response.json().then((data) => {
                 if (data.error) {
